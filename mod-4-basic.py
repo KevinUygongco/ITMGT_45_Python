5c5a8f41fb132f292d2c1ac9b1b66f3894e6ec35
KevinUygongco <123729007+KevinUygongco@users.noreply.github.com>
2023-04-26 18:58:52 +0800

    Update mod-4-basic.py

--- mod-4-basic.py
+++ mod-4-basic.py
@@ -33,7 +33,13 @@ def savings(gross_pay, tax_rate, expenses):
     '''
     # Replace `pass` with your code. 
     # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+
+    import math
+    after_tax = (gross_pay) - gross_pay*tax_rate
+    math.floor(after_tax)
+    savings = after_tax - expenses
+    
+    return savings
 
 def material_waste(total_material, material_units, num_jobs, job_consumption):
     '''Material Waste.
